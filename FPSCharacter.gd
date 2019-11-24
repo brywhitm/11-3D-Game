bfc8533391d64136727794a3f677aae777082401
brywhitm <brywhitm@iu.edu>
2019-11-23 20:35:18 -0500

    xp/health

--- FPSCharacter.gd
+++ FPSCharacter.gd
@@ -12,6 +12,10 @@ var jump=false
 
 var velocity = Vector3()
 
+signal health_changed(health)
+signal health_depleted
+var health = 0
+
 func get_input():
     jump=false
     if Input.is_action_just_pressed("jump"):
@@ -45,3 +49,56 @@ func _physics_process(delta):
     velocity = move_and_slide(velocity, Vector3.UP, true)
     if jump and is_on_floor():
         velocity.y=jump_speed
+
+signal experience_gained(growth_data)
+
+# CHARACTER STATS
+export(int) var max_hp = 12
+export(int) var strength = 8
+export(int) var magic = 8
+
+# LEVELING SYSTEM
+export (int) var level = 1
+
+var experience = 0
+var experience_total = 0
+var experience_required = get_required_experience(level + 1)
+
+func get_required_experience(level):
+	return round(pow(level, 1.8) + level * 4)
+
+func gain_experience(amount):
+	experience_total += amount
+	experience += amount
+	var growth_data = []
+	while experience >= experience_required:
+		experience -= experience_required
+		growth_data.append([experience_required, experience_required])
+		level_up()
+	growth_data.append([experience, get_required_experience(level + 1)])
+	emit_signal("experience_gained", growth_data)
+
+func level_up():
+	level += 1
+	experience_required = get_required_experience(level + 1)
+
+	var stats = ['max_hp', 'strength', 'magic']
+	var random_stat = stats[randi() % stats.size()]
+	set(random_stat, get(random_stat) + randi() % 4)
+	
+	health = max_hp
+	emit_signal("health_changed", health)
+
+func _ready():
+	health = max_hp
+	emit_signal("health_changed", health)
+
+func take_damage(amount):
+	health -= amount
+	health = max(0, health)
+	emit_signal("health_changed", health)
+
+func heal(amount):
+	health += amount
+	health = max(health, max_hp)
+	emit_signal("health_changed", health)
--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -1,24 +1,14 @@
-extends Spatial
+extends Node
 
-onready var _character = $FPSCharacter
+onready var _character = $Character
 onready var _label = $Interface/Label
 onready var _bar = $Interface/ExperienceBar
 
-
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	_label.update_text(_character.level, _character.experience, _character.experience_required)
 	_bar.initialize(_character.experience, _character.experience_required)
 
-
 func _input(event):
-	if event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if event.is_action_pressed("shoot"):
-		if Input.get_mouse_mode()==Input.MOUSE_MODE_VISIBLE:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			get_tree().set_input_as_handled()
-
 	if not event.is_action_pressed('ui_accept'):
 		return
 	_character.gain_experience(34)
